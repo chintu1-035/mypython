170352a4d1b5e9c368b53b5cde6946a383323d7b
chintu1-035 <uppiupendra701@gmail.com>
2025-03-21 15:37:36 +0530

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,81 @@
+'''a=25
+b=35
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+mod=a%b
+fd=a//b
+ex=a**b
+print(add)
+print(sub)
+print(mul)
+print(div)
+print(mod)
+print(fd)
+print(ex)'''
+
+'''a=int(input("enter first number:"))
+b=int(input("enter second number:"))
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+mod=a%b
+fd=a//b
+ex=a**b
+print(add)
+print(sub)
+print(mul)
+print(div)
+print(mod)
+print(fd)
+print(ex)'''
+
+'''a=int(input("enter first number:"))
+b=int(input("enter second number:"))
+print(a==b)
+print(a!=b)
+print(a<b)
+print(a>b)
+print(a<=b)
+print(a>=b)'''
+
+'''a=7
+b=9
+print("before swapping:",a,b)
+a=a+b
+b=a-b
+a=a-b
+print("after swapping:",a,b)'''
+
+'''a,b=7,9
+print("before swapping:",a,b)
+a,b=b,a
+print("after swapping:",a,b)'''
+
+'''a=6
+b=8
+print("before swapping:",a,b)
+a=a*b
+b=a//b
+a=a//b
+print("after swapping:",a,b)'''
+
+'''base=float(input("base:"))
+height=float(input("height:"))
+area=0.3*base*height
+print("area of triangle:",area)'''
+
+'''length=float(input("enter the length of the rectangle:"))
+width=float(input("enter the width of the rectangle:"))
+area=length*width
+perimeter=2*(length+width)
+print("area of rectangle is",area)
+print("perimeter of rectangle is",perimeter)'''
+
+import math
+radius=float(input("enter the radius of the circle:"))
+area=math.pi*radius*radius
+print("area of the circle is:{0}",format(area))
+
