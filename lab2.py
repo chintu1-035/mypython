3686a56400ae40cd770f6d272c8aeee42e7042e5
chintu1-035 <uppiupendra701@gmail.com>
2025-03-28 15:24:45 +0530

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,72 @@
+'''name=input("enter your name:")
+age=int(input("enter your age:"))
+if age>=18:
+    print("congratulations",name+",you are eligible to vote!")'''
+
+'''num=int(input("enter a number:"))
+if num%2==0:
+    print(num,"is even.")
+else:
+    print(num,"is odd.")'''
+
+'''num=int(input("enter a number:"))
+if num==0:
+    print("zero")
+elif num==1:
+    print("one")
+elif num==2:
+    print("two")
+elif num==3:
+    print("three")
+elif num==4:
+    print("four")
+elif num==5:
+    print("five")
+elif num==6:
+    print("six")
+elif num==7:
+    print("seven")
+elif num==8:
+    print("eight")
+elif num==9:
+    print("nine")
+else:
+    print("number out of range.")'''
+
+'''num=int(input("enter a number:"))
+if num>=1 and num<=100:
+    print("the number",num,"is between number")
+else:
+    print("the number",num,"is not between number")'''
+
+'''N=int(input("enter the value of N:"))
+count=0
+num=5
+while count<N:
+    print(num)
+    num+=5
+    count+=1'''
+
+'''mylist=input("enter a list of numbers seperated by space:")
+mylist=list(map(int,mylist.split()))
+sum=0
+for num in mylist:
+    sum+=num
+    print("the sum of the numbers is:",sum)'''
+
+'''for i in range(1,11):
+    if i==5:
+        continue
+    if i==8:
+        break
+    print(i)'''
+
+'''num=0
+while num<=20:
+    if num%2==0:
+        print(num,end=",")
+    num+=1'''
+          
+    
+
+    
