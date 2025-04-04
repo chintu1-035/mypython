20cbc9fa78c1868c67c3290d3b849866d27288d9
chintu1-035 <uppiupendra701@gmail.com>
2025-04-04 15:28:19 +0530

    Add files via upload

---
+++ lab3.py
@@ -0,0 +1,58 @@
+'''my_list=[]
+n=int(input("enter any number:"))
+for x in range(n):
+    num=int(input("enter a number:"))
+    my_list.append(num)
+    print("my updated list is:",my_list)'''
+
+'''l=[10,20,30,"milky",(10+20j),True,'l']
+print(l[3])
+print(l[-2])
+print(l[1:4])
+print(l)
+l.remove(30)
+print(l)'''
+
+'''l=[10,20,30,40,60,True,'l']
+print(len(l))
+print(l)
+print(max(l))
+print(min(l))'''
+
+'''my_list=[8,2,3,0,7]
+sum=0
+for i in my_list:
+    sum=sum+i
+    print("the sum is:",sum)'''
+
+'''s=input("enter the string:")
+print(s.split())'''
+
+'''n=int(input("enter the value of n:"))
+my_list=[]
+for i in range(n):
+    num=int(input("enter a number:"))
+    my_list.append(num)
+    print("even numbers are:")
+    print("[",end=" ")
+    for num in my_list:
+        if num%2==0:
+            print(num,end=",")
+            print("]")'''
+
+'''list=[10,20,30,"milky",True,0,(10+20j),False,'l',True]
+print(list)
+x=list.copy()
+print(x)
+list.pop(3)
+print(list)
+del list[2]
+print(list)
+list.reverse()
+print(list)
+list.clear()
+print(list)'''
+
+
+    
+            
