ea1fb81725c3666ac7380c963f70daf2e18b6379
chintu1-035 <uppiupendra701@gmail.com>
2025-05-30 16:29:46 +0530

    Add files via upload

---
+++ lab6.py
@@ -0,0 +1,48 @@
+'''import turtle
+t=turtle.Turtle()
+t.forward(100)
+turtle.mainloop()'''
+
+'''import turtle
+t=turtle.Turtle()
+t.circle(50)
+turtle.mainloop()'''
+
+'''import turtle
+t=turtle.Turtle()
+turtle.bgcolor("red")
+turtle.mainloop()'''
+
+'''import turtle
+my_pen=turtle.Turtle()
+for i in range(4):
+    mypen.forward(50)
+    my_pen.right(90)
+    turtle.done()'''
+
+'''import turtle
+my_pen=turtle.Turtle()
+for i in range(50):
+    my_pen.forward(50)
+    my_pen.right(144)
+    turtle.done()'''
+
+'''import turtle
+polygon=turtle.Turtle()
+my_num_side=6
+my_side_length=70
+my_angle=360.0/my_num_side
+for i in range(my_num_side):
+    polygon.forward(my_side_length)
+    polygon.right(my_angle)
+    turtle.done()'''
+
+'''import turtle
+colors=["red","purple","blue","green","orange","yellow"]
+my_pen=turtle.pen()
+turtle.bgcolor("black")
+for x in range(360):
+    my_pen.pencolor(colors[x%6])
+    my_pen.width(x/100+1)
+    my_pen.forward(x)
+    my_pen.left(59)'''
