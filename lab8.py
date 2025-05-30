c419cf2f223f2dd86228726e478eb23ab4db4400
chintu1-035 <uppiupendra701@gmail.com>
2025-05-30 16:31:00 +0530

    Add files via upload

---
+++ lab8.py
@@ -0,0 +1,40 @@
+'''import random
+random_number=random.randint(1,10)
+print("random number:",random_number)'''
+
+'''import datetime
+current_datetime=datetime.datetime.now()
+print("current date and time:",current_datetime)'''
+
+'''import math
+number=25
+sqrt=math.sqrt(number)
+print("square root:",sqrt)'''
+
+'''import math
+factorial=math.factorial(5)
+print("factorial:",factorial)'''
+
+'''file_path="example.txt"
+with open(file_path,"w")as file:
+    file.write("hello,world!")
+with open(file_path,"r")as file:
+    content=file.read()
+    print("file content:",content)'''
+
+'''import string
+text="hello,world!"
+Uppercase_text=text.upper()
+print("Uppercase text:",Uppercase_text)'''
+
+'''import secrets
+random_number=secrets.randbelow(10)+1
+print("cryptographically secure random number:",random_number)'''
+
+'''import platform
+system_name=platform.system()
+node_name=platform.node()
+processor_name=platform.processor()
+print("system name:",system_name)
+print("node name:",node_name)
+print("processor name:",processor_name)'''
