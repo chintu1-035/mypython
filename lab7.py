03371b89e01c7084a63178f50273dddfc3122291
chintu1-035 <uppiupendra701@gmail.com>
2025-05-30 16:30:36 +0530

    Add files via upload

---
+++ lab7.py
@@ -0,0 +1,39 @@
+'''class car:
+    def __init__(self ,brand,speed):
+        self.brand=brand
+        self.speed=speed
+    def display(self):
+        print(f"brand:{self.brand},speed:{self.speed}km/h")
+    def create_car():
+        car1=car("TOYOTA",180)
+        return car1
+my_car=car.create_car()
+my_car.display()'''
+
+'''class student:
+    def __init__(self ,name,marks):
+        self.name=name
+        self.marks=marks
+def print_student_info(student):
+    print(f"name:{student.name},marks:{student.marks}")
+s1=student("tillu",92)
+print_student_info(s1)'''
+
+'''class circle:
+    def __init__(self,radius):
+        self.radius=radius
+    def area(self):
+        return 3.14*self.radius*self.radius
+def get_circle(r):
+    return circle(r)
+c=get_circle(5)
+print("area of circle:",c.area())'''
+  
+'''class book:
+    def __init__(self,title):
+        self.title=title
+def create_book_list():
+    return[book("python 101"),book("ai basics"),book("data science")]
+book=create_book_list()
+for b in book:
+    print("book title:",b.title)'''
